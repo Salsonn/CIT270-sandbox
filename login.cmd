8052ca6d591a4b72461caf4fffa48063d62ed49b
Caleb Salyards <34790708+Salsonn@users.noreply.github.com>
2023-04-28 20:26:57 -0600

    Week 2 update

--- login.cmd
+++ login.cmd
@@ -1,1 +1,4 @@
-echo "Logging in"
+@echo off
+echo Logging in
+curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
+curl https://dev.stedi.me/validate/4465b669-49b4-4e07-961f-8a4a5211c363
